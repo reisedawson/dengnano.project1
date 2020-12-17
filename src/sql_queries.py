56e5060cac1e9fc301c5a30138064a4c3ee304b1
Reise Dawson <reisedawson@gmail.com>
2020-12-17 17:24:39 +0000

    created each individual sql script and changed sql_queries.py to read from them

--- src/sql_queries.py
+++ src/sql_queries.py
@@ -1,45 +1,26 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = open('sql/songplay_table_drop.sql', 'r').read()
+user_table_drop = open('sql/user_table_drop.sql', 'r').read()
+song_table_drop = open('sql/song_table_drop.sql', 'r').read()
+artist_table_drop = open('sql/artist_table_drop.sql', 'r').read()
+time_table_drop = open('sql/time_table_drop.sql', 'r').read()
 
 # CREATE TABLES
 
-songplay_table_create = ("""
-""")
-
-user_table_create = ("""
-""")
-
-song_table_create = ("""
-""")
-
-artist_table_create = ("""
-""")
-
-time_table_create = ("""
-""")
+songplay_table_create = open('sql/songplay_table_create.sql', 'r').read()
+user_table_create = open('sql/user_table_create.sql', 'r').read()
+song_table_create = open('sql/song_table_create.sql', 'r').read()
+artist_table_create = open('sql/artist_table_create.sql', 'r').read()
+time_table_create = open('sql/time_table_create.sql', 'r').read()
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""
-""")
-
-user_table_insert = ("""
-""")
-
-song_table_insert = ("""
-""")
-
-artist_table_insert = ("""
-""")
-
-
-time_table_insert = ("""
-""")
+songplay_table_insert = open('sql/songplay_table_insert.sql', 'r').read()
+user_table_insert = open('sql/user_table_insert.sql', 'r').read()
+song_table_insert = open('sql/song_table_insert.sql', 'r').read()
+artist_table_insert = open('sql/artist_table_insert.sql', 'r').read()
+time_table_insert = open('sql/time_table_insert.sql', 'r').read()
 
 # FIND SONGS
 
@@ -48,5 +29,8 @@ song_select = ("""
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [songplay_table_create, user_table_create, /
+                        song_table_create, artist_table_create, /
+                        time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, /
+                      artist_table_drop, time_table_drop]
