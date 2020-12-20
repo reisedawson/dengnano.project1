baed3683c9c5338fae98e9aaf47bb18e3d1d3f28
Reise Dawson <reisedawson@gmail.com>
2020-12-20 17:52:37 +0000

    changed constraints on fact table from NOT NULL to foreign keys (REFERENCES) for song and artist id to allow NULLS due to sampled dataset

--- sql/songplay_table_create.sql
+++ sql/songplay_table_create.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS songplays (
     , start_time timestamptz NOT NULL
     , user_id integer NOT NULL
     , user_level varchar -- renamed from level due to keyword conflict
-    , song_id varchar NOT NULL
-    , artist_id varchar NOT NULL
+    , song_id varchar REFERENCES songs
+    , artist_id varchar REFERENCES artists
     , session_id integer
     , songplay_location varchar -- renamed from location due to keyword conflict
     , user_agent varchar
