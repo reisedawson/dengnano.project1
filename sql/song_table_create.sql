1e067ee45876eb4d899565de6f641d8490529fbd
Reise Dawson <reisedawson@gmail.com>
2020-12-21 16:53:33 +0000

    removing foreign key constraint from songs table referencing artists as it makes the star schema look messy. If I was creating the star schema for real I would model it slightly differently anyway.

--- sql/song_table_create.sql
+++ sql/song_table_create.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS songs (
     song_id varchar NOT NULL PRIMARY KEY
-    , artist_id varchar NOT NULL REFERENCES artists
+    , artist_id varchar NOT NULL
     , title varchar NOT NULL
     , song_year integer -- renamed from year due to keyword conflict
     , duration numeric
